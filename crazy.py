77d0fc854189a7ebbd3f4ee8a1df592df0af5aa6
vdtp69-dev <vdtp69@gmail.com>
2026-06-19 14:17:29 +0530

    Complete contact book with CRUD and file persistence

--- crazy.py
+++ crazy.py
@@ -1,5 +1,74 @@
-contacts={}
-count=0
+def load_contacts():
+    numb={}
+    try:
+        file =open("contacts.txt","r")
+        for line in file:
+            name,number=line.strip().split(",")
+            numb[name]=number
+        file.close()
+        return numb
+        
+        
+    except FileNotFoundError:
+        print("File doesnt exist!")
+        return numb
+    
+def save_contacts(contacts):
+    file = open("contacts.txt","w")
+    for name,number in contacts.items():
+        file.write(f"{name},{number}\n")
+    file.close()
+        
+contacts=load_contacts()   
+    
+def add_contacts(contacts):
+       name= input("Enter your name:")
+       number=input("Enter your number: ")
+       file=open("contacts.txt","a")
+       file.write(f"{name},{number}\n")
+       file.close()
+       contacts[name]=number
+       print("Contact added!")
+       
+       
+def view_contacts(contacts):
+    if len(contacts)>0:
+        for name, number in contacts.items():
+            print(f"{name}: {number}")
+    else:
+        print("Currently you dont have any contacts in your mini contact book")
+        
+        
+def search_contacts(contacts):
+    search=input("\nEnter the contact name you want to search: ")
+    if search in contacts:
+        print(f"{search}'s phone number is {contacts[search]}")
+    else:
+        print("Contact not found!")
+        
+def delete_contact(contacts):
+    name = input("Enter contact name to delete: ")
+
+    if name in contacts:
+        del contacts[name]
+        print("Contact deleted!")
+    else:
+        print("Contact not found!")
+    
+    save_contacts(contacts)
+            
+def update_contact(contacts):    
+    name=input("Enter the contact name you want to update: ")
+    if name in contacts:
+        contacts[name]=input("Enter the new contact number: ")
+        save_contacts(contacts)
+    else:
+        a=input(("Do you want to create a new contact ? y or n?"))
+        if(a=='y'):
+            add_contacts(contacts)
+        else:
+            print("Okay")
+    
 while True:
     print("\n1.Add contact: ")
     print("2.Search contact: ")
@@ -12,41 +81,27 @@ while True:
     choice=input("Enter your choice: ")
 
     if choice == "1" :
-       name= input("Enter your name:")
-       contacts[name]=input("Enter your number: ")
-       print("Contact added!")
+       add_contacts(contacts)
        
     elif choice == "2":
-        search=input("\nEnter the contact name you want to search: ")
-        if search in contacts:
-            print(f"{search}'s phone number is {contacts[search]}")
-        else:
-            print("Contact not found!")
+        search_contacts(contacts)
             
     elif choice == "3":
-       for name, number in contacts.items():
-            print(f"{name}: {number}")
+       view_contacts(contacts)
             
     elif choice == "4":
         print("Goodbye!")
         break
     
     elif choice == "5":
-        name = input("Enter contact name to delete: ")
-
-        if name in contacts:
-            del contacts[name]
-            print("Contact deleted!")
-        else:
-            print("Contact not found!")
+        delete_contact(contacts)
             
     elif choice == "6":
         print(f"There are {len(contacts)} contacts present in your Mini Contact Book")
     
     elif choice == "7":
-        name=input("Enter the contact name you want to update: ")
-        contacts[name]=input("Enter the new contact number: ")
-    
+        update_contact(contacts)
+        
     else:
         print("Invalid Choice")
         
