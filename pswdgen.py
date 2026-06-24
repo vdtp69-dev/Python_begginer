4bdfc3953881d2e5d7a6fd2e822f051756d034d4
vdtp69-dev <vdtp69@gmail.com>
2026-06-24 13:14:40 +0530

    Password generator v2

--- pswdgen.py
+++ pswdgen.py
@@ -1,18 +1,24 @@
 import random
-def password_generator(a):  
+def password_generator(a,b):  
     password=''
-    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+    if b.lower() == "y":
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*"
+        
+    else:
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+      
     if a > 0:
         for i in range(a):
             password+=random.choice(chars)
         return password
-        
+            
     else:
         print("Nothing will be generated")
-  
 
 a=int(input("Enter the length of your password: "))
-
-password=password_generator(a)
-
-print(f"Your password is: {password}")
+b=input("Include Special Characters? (y/n)")
+c=int(input("How many passwords?"))
+print("Generated passwords:")
+for i in range(c):
+    password=password_generator(a,b)
+    print(f"\n\n{i+1}.{password}")
